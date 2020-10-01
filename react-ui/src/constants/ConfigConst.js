354b2fe018925742f9cb0604526a3fe665f03055
tquangdo <trangia61@gmail.com>
2020-10-01 16:25:40 +0900

    1

---
+++ react-ui/src/constants/ConfigConst.js
@@ -0,0 +1,2 @@
+export const API_URL = (process.env.NODE_ENV === 'production') ? `https://${process.env.REACT_APP_DOMAINNAME}/api` : 'http://localhost:5000/api'
+export const GG_CLIENT_ID = "158263387547-mnpump8ga52vogclo7c86v6g3j0h2ejl.apps.googleusercontent.com"
--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const routes = require('./routes/')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const helmet = require('helmet')
+// const helmet = require('helmet')
 // const cloudinary = require('cloudinary')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
@@ -32,7 +33,7 @@ try {
     console.log(error)
 }
 
-const port = 5000 || process.env.PORT
+const port = process.env.PORT || 5000
 
 /** set up routes {API Endpoints} */
 routes(router)
@@ -40,11 +41,15 @@ routes(router)
 /** set up middlewares */
 app.use(cors())
 app.use(bodyParser.json())
-app.use(helmet())
+// app.use(helmet())
 
 app.use('/api', router)
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')))
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'))
+})
 
 /** start server */
 app.listen(port, () => {
-    console.log(`Server started at port: ${port}`);
-});
+    console.log(`Express server started at port: ${port}`)
+})
